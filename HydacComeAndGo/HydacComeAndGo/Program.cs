9f82004a844fb9bdf8bc727266a06990aa33523a
ToothbrushPenguin <emil.k.m@hotmail.dk>
2024-09-11 07:03:24 +0200

    add files

---
+++ HydacComeAndGo/HydacComeAndGo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HydacComeAndGo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
