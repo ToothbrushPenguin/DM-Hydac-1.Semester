48b162172ed6bd067ed51faad2650cf60f4a5a56
Asbjørn <asbjrasm123@gmail.com>
2024-10-01 15:07:09 +0200

    gust class added

--- HydacComeAndGo/HydacComeAndGo/obj/Release/net8.0/HydacComeAndGo.AssemblyInfo.cs
+++ HydacComeAndGo/HydacComeAndGo/obj/Release/net8.0/HydacComeAndGo.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HydacComeAndGo")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f82004a844fb9bdf8bc727266a06990aa33523a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HydacComeAndGo")]
