70a653f677d7bd68caee59b140fceb18fae8b669
RayvenDK <dennis99@live.dk>
2024-10-01 15:07:33 +0200

    til føjet klassen log

---
+++ HydacComeAndGo/HydacComeAndGo/Log.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydacComeAndGo
+{
+    internal class Log
+    {
+        private DateTime arrival;
+        public DateTime Arrival
+        {
+            get { return arrival; }
+            set { arrival = value; }
+        }
+
+        private DateTime departuer;
+        public DateTime Departuer
+        {
+            get { return departuer; }
+            set { departuer = value; }
+        }
+
+        private string room;
+        public string  Room
+        {
+            get { return room; }
+            set { room = value; }
+        }
+
+
+    }
+}
